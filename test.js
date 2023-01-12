d584f7cf7cd35e5325c41bb4df8f7b734b2d99df
Pauline Muigai <wanjirumuigai@gmail.com>
2023-01-12 13:16:23 +0300

    completed assignment

--- test.js
+++ test.js
@@ -1,17 +1,9 @@
-class Formatter {
-  //add static methods here
-  static capitalize(string) {
-     return string.charAt(0).toUpperCase() + string.slice(1);
-  }
-  static sanitize(string) {
-    return string.replace(/[^A-Za-z0-9- ']+/g, "");
-  }
-  static titleize(string) {
+function titleize(string) {
     let conjunctions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
     string = string.toLowerCase()
     string = string.split(' ')
      
-    
+    console.log(string)
     for(let i=0;i<string.length;i++) {
         if(!conjunctions.includes(string[i])) {
         string[i] = string[i].charAt(0).toUpperCase() + string[i].slice(1)
@@ -20,7 +12,7 @@ class Formatter {
   }
   string = string.join(' ')
   string = string.charAt(0).toUpperCase() + string.slice(1)
-  return string
+  console.log(string)
   
 }
-}
+titleize('Getting giggles')
